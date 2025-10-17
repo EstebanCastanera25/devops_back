85ea000f56ea442e6bb0d809cbd4b71028926889
Esteban <estebancastanera@gmail.com>
2025-10-17 19:03:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ async function startServer () {
   await connectDB()
 
   app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+    console.log(`Servidor corriendo`)
   })
 }
 startServer()
