9704cf3a0655293b73d65ccac17ecb69583a2551
Esteban <estebancastanera@gmail.com>
2025-10-17 19:03:32 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 export async function connectDB () {
   try {
     await mongoose.connect(process.env.MONGO_URI)
-    console.log('MONGO_URI=', process.env.MONGO_URI);
     console.log('Conectado a MongoDB Atlas')
   } catch (err) {
     console.error('Error de conexión a MongoDB:', err.message)
